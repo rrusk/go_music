d64fc746cc89504d4c33f4d53bffd4b597bd9652
Raymond Rusk <Raymond.Rusk@shaw.ca>
2024-12-07 14:18:46 -0800

    support flac, ogg and wav

--- music.go
+++ music.go
@@ -1,48 +1,81 @@
 package main
 
 import (
-	"github.com/faiface/beep"
-	"github.com/faiface/beep/mp3"
-	"github.com/faiface/beep/speaker"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/gopxl/beep/v2"
+	"github.com/gopxl/beep/v2/flac"
+	"github.com/gopxl/beep/v2/mp3"
+	"github.com/gopxl/beep/v2/speaker"
+	"github.com/gopxl/beep/v2/vorbis"
+	"github.com/gopxl/beep/v2/wav"
 	fyne "fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-	"os"
-	"time"
 )
 
 func main() {
+	// Check if a file path is provided as a command-line argument
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go_music <file-path>")
+		return
+	}
+
+	audioFilePath := os.Args[1]
+
 	// Initialize Fyne app
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Music Player")
 
 	// Play button
 	playButton := widget.NewButton("Play", func() {
-		go playAudio("example.mp3") // Run audio playback in a separate goroutine
+		go playAudio(audioFilePath) // Play the file provided via the command line
 	})
 
 	// Add play button to window
-	myWindow.SetContent(container.NewVBox(playButton))
-    myWindow.Resize(fyne.NewSize(300, 150))
+	myWindow.SetContent(container.NewVBox(
+		widget.NewLabel(fmt.Sprintf("File: %s", filepath.Base(audioFilePath))),
+		playButton,
+	))
+	myWindow.Resize(fyne.NewSize(300, 150))
 	myWindow.ShowAndRun()
 }
 
 func playAudio(filePath string) {
-	// Open the MP3 file
+	// Open the audio file
 	file, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	// Decode MP3 and initialize speaker
-	streamer, format, err := mp3.Decode(file)
+	// Decode audio based on file extension
+	var streamer beep.StreamSeekCloser
+	var format beep.Format
+	ext := filepath.Ext(filePath)
+
+	switch ext {
+	case ".mp3":
+		streamer, format, err = mp3.Decode(file)
+	case ".wav":
+		streamer, format, err = wav.Decode(file)
+	case ".ogg":
+		streamer, format, err = vorbis.Decode(file)
+	case ".flac":
+		streamer, format, err = flac.Decode(file)
+	default:
+		panic("Unsupported audio format: " + ext)
+	}
 	if err != nil {
 		panic(err)
 	}
 	defer streamer.Close()
 
+	// Initialize speaker
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	// Play the audio in a blocking manner
