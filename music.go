18fb263f18df35c4e82656b1d2defde0e46d26c6
Raymond Rusk <Raymond.Rusk@shaw.ca>
2024-12-14 15:36:44 -0800

    create playlist from music in folder

--- music.go
+++ music.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-
 	"time"
 
 	fyne "fyne.io/fyne/v2"
@@ -27,60 +26,94 @@ var (
 	done                 chan bool
 	playPauseButton      *widget.Button
 	restartButton        *widget.Button
+	nextButton           *widget.Button
+	prevButton           *widget.Button
 	progressBar          *widget.ProgressBar
 	currentPositionLabel *widget.Label
 	totalDurationLabel   *widget.Label
+	currentSongLabel     *widget.Label
+
+	playlist         []string
+	currentSongIndex int
 
 	defaultBufferLength = 100
 	defaultSampleRate   = 44100
 )
 
 func main() {
-	// Check if a file path is provided as a command-line argument
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: go_music <file-path>")
+	// Define the directory containing the songs
+	directory := "/home/rrusk/Music/MUSICCOMP"
+
+	// Initialize the playlist
+	playlist = []string{}
+	if err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			switch filepath.Ext(path) {
+			case ".mp3", ".wav", ".ogg", ".flac":
+				playlist = append(playlist, path)
+			}
+		}
+		return nil
+	}); err != nil {
+		fmt.Println("Error reading directory:", err)
 		return
 	}
 
-	audioFilePath := os.Args[1]
+	if len(playlist) == 0 {
+		fmt.Println("No supported audio files found in directory")
+		return
+	}
 
 	// Init soundcard
 	var sampleRate beep.SampleRate = beep.SampleRate(defaultSampleRate)
 	speaker.Init(sampleRate, sampleRate.N(time.Duration(defaultBufferLength)*time.Millisecond))
-	go playAudio(audioFilePath)
 
 	// Initialize Fyne app
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Music Player")
 
-	// Play/Pause button
+	// UI Elements
 	playPauseButton = widget.NewButton("Pause", func() {
 		go togglePlayPause()
 	})
 
-	// Restart button
 	restartButton = widget.NewButton("Restart", func() {
 		go restartPlayback()
 	})
 
-	// Progress bar
+	nextButton = widget.NewButton("Next", func() {
+		go playNextSong()
+	})
+
+	prevButton = widget.NewButton("Previous", func() {
+		go playPreviousSong()
+	})
+
 	progressBar = widget.NewProgressBar()
 
-	// Time labels
 	currentPositionLabel = widget.NewLabel("Current: 00:00")
 	totalDurationLabel = widget.NewLabel("Total: 00:00")
+	currentSongLabel = widget.NewLabel("Now Playing: " + filepath.Base(playlist[0]))
 
-	// Add widgets to the window
+	// Layout
 	myWindow.SetContent(container.NewVBox(
-		widget.NewLabel(fmt.Sprintf("File: %s", filepath.Base(audioFilePath))),
+		currentSongLabel,
 		playPauseButton,
 		restartButton,
+		container.NewHBox(prevButton, nextButton),
 		progressBar,
 		container.NewHBox(currentPositionLabel, totalDurationLabel),
 	))
 
 	myWindow.Resize(fyne.NewSize(400, 200))
 	myWindow.Show()
+
+	// Start playing the first song
+	currentSongIndex = 0
+	go playAudio(playlist[currentSongIndex])
 	myApp.Run()
 }
 
@@ -139,18 +172,33 @@ func playAudio(filePath string) {
 	})))
 
 	playingSong = true
+	currentSongLabel.SetText("Now Playing: " + filepath.Base(filePath))
 	go updateProgressBar()
 
 	<-done
 
 	playingSong = false
 	playPauseButton.SetText("Play")
+	playNextSong() // Automatically play the next song when current one ends
+}
+
+func playNextSong() {
+	if currentSongIndex < len(playlist)-1 {
+		currentSongIndex++
+		go playAudio(playlist[currentSongIndex])
+	}
+}
+
+func playPreviousSong() {
+	if currentSongIndex > 0 {
+		currentSongIndex--
+		go playAudio(playlist[currentSongIndex])
+	}
 }
 
 func restartPlayback() {
 	speaker.Lock()
 	if musicStreamer != nil {
-
 		if err := musicStreamer.Seek(0); err != nil {
 			fmt.Println("Error restarting playback:", err)
 			return
@@ -187,5 +235,4 @@ func updateProgressBar() {
 		}
 		speaker.Unlock()
 	}
-
 }
